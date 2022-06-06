06dda9f25e8ddb02d9585a2185b70ca0e5172cbf
engayeshach <engayeshach@gmail.comm>
2022-06-05 23:51:38 -0700

    admin-dashboard completed

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,17 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // admin routes
-Route::get('backend/index', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('backend.index')->middleware(['auth', 'isAdmin']);
-Route::get('category/create',[App\Http\Controllers\CategoryController::class, 'create'])->name('category.create');
-Route::get('categories',[App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
-Route::get('category/edit{category}',[App\Http\Controllers\CategoryController::class, 'edit'])->name('category.edit');
-Route::post('category/update',[App\Http\Controllers\CategoryController::class, 'update'])->name('category.update');
-Route::get('category/delete{category}',[App\Http\Controllers\CategoryController::class, 'destroy'])->name('category.destroy');
-Route::post('category/store',[App\Http\Controllers\CategoryController::class, 'store'])->name('category.store');
+Route::middleware('auth')->group(function(){
+    Route::get('backend/index', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('backend.index')->middleware(['auth', 'isAdmin']);
+    Route::get('categories',[App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
+        Route::prefix('category')->group(function(){
+            Route::get('category/create',[App\Http\Controllers\CategoryController::class, 'create'])->name('category.create');
+            Route::get('category/edit{category}',[App\Http\Controllers\CategoryController::class, 'edit'])->name('category.edit');
+            Route::post('category/update',[App\Http\Controllers\CategoryController::class, 'update'])->name('category.update');
+            Route::get('category/delete{category}',[App\Http\Controllers\CategoryController::class, 'destroy'])->name('category.destroy');
+            Route::post('category/store',[App\Http\Controllers\CategoryController::class, 'store'])->name('category.store');
+        });
+
+});
+
+
